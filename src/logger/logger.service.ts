da502dd785c286a518949a00a2167d8fb0d40559
DURAND Malo <malo.durand@epitech.eu>
2025-03-30 04:24:30 +0200

    fix: untracked logger module.

---
+++ src/logger/logger.service.ts
@@ -0,0 +1,42 @@
+import { LoggerService as LS } from '@nestjs/common';
+import * as winston from 'winston';
+import { utilities as nestWinstonModuleUtilities } from 'nest-winston';
+
+export default class LoggerService implements LS {
+  private readonly logger: winston.Logger = winston.createLogger({
+    transports: [
+      new winston.transports.Console({
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          winston.format.ms(),
+          nestWinstonModuleUtilities.format.nestLike('Nest', {
+            colors: true,
+            prettyPrint: true,
+            processId: true,
+            appName: true,
+          }),
+        ),
+      }),
+    ],
+  });
+
+  log(message: string) {
+    this.logger.info(message);
+  }
+
+  error(message: string, trace: string) {
+    this.logger.error(message, trace);
+  }
+
+  warn(message: string) {
+    this.logger.warn(message);
+  }
+
+  debug(message: string) {
+    this.logger.debug(message);
+  }
+
+  verbose(message: string) {
+    this.logger.debug(message);
+  }
+}
